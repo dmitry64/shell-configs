e0c0751f5c1d43e21bbff74a5a85add99a2d1c73
Dmitry Zykov <dmitriizykov64@gmail.com>
2016-07-31 15:01:33 +0300

    Base configs

---
+++ .vimrc
@@ -0,0 +1,6 @@
+set background=dark
+colorscheme deep-space
+syntax on
+set t_Co=256
+set rtp+=/usr/lib/python3.5/site-packages/powerline/bindings/vim/
+set laststatus=2
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ./.vimrc ~/.vimrc
+cp -R ./.vim/ ~/.vim
+cp ./.tmux.conf ~/.tmux.conf
+cp -R ./.tmux-themepack ~/.tmux-themepack
+cp -R ./.tmux/ ~/.tmux/
+cp ./.zshrc ~/.zshrc
+cp -R ./.oh-my-zsh ~/.oh-my-zsh
